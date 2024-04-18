81c6316a175056beaf7278035853021bd211dda9
minerBRUH <112920598+macalalayjc@users.noreply.github.com>
2024-04-18 10:30:10 +0800

    URLModal animation added

--- frontend/src/components/UrlModal.jsx
+++ frontend/src/components/UrlModal.jsx
@@ -1,4 +1,38 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+`;
+const ModalContainer = styled(motion.div)`
+  width: 80%; /* Adjust width as needed */
+  max-width: 70vw; /* Set maximum width if necessary */
+  height: 70vh; /* Allow height to adjust based on content */
+  max-height: 80vh; /* Set maximum height if necessary */
+  overflow: auto; /* Add scrollbar if content overflows */
+  background-color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 12px;
+`;
+const modalVariant = {
+  initial: { opacity: 0 },
+  isOpen: { opacity: 1 },
+  exit: { opacity: 0 }
+};
+const containerVariant = {
+  initial: { top: "-50%", transition: { type: "spring" } },
+  isOpen: { top: "50%" },
+  exit: { top: "-50%" }
+};
 
 const UrlModal = ({ isOpen, onClose, url }) => {
   const modalRef = useRef(null);
@@ -21,24 +55,34 @@ const UrlModal = ({ isOpen, onClose, url }) => {
     };
   }, [isOpen, onClose]);
 
-  if (!isOpen) return null;
-//
   return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex">
-      <div ref={modalRef} className="relative p-8 bg-white w-3/4 h-3/4 max-w-3xl max-h-3xl m-auto flex-col flex rounded-md">
-        <button
-          className="absolute top-4 right-4 text-xl text-gray-600"
-          onClick={onClose}
+    <AnimatePresence>
+      {isOpen && (
+        <Overlay
+          initial={"initial"}
+          animate={"isOpen"}
+          exit={"exit"}
+          variants={modalVariant}
         >
-          &times;
-        </button>
-        <iframe
-          src={url}
-          title="Modal Content"
-          className="w-full h-full"
-        ></iframe>
-      </div>
-    </div>
+          <ModalContainer variants={containerVariant} ref={modalRef}>
+            <div className="relative p-8 bg-white w-fulll h-full max-w-3xl max-h-3xl m-auto flex-col flex rounded-md transition-opacity duration-300 ease-in-out">
+              <button
+                className="absolute top-4 right-4 text-xl text-gray-600"
+                onClick={onClose}
+              >
+                &times;
+              </button>
+              <iframe
+                src={url}
+                title="Modal Content"
+                className="w-full h-full"
+                style={{ border: 'none' }} // Remove iframe border if necessary
+              ></iframe>
+            </div>
+          </ModalContainer>
+        </Overlay>
+      )}
+    </AnimatePresence>
   );
 };
 
