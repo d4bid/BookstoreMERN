0e55730f6c4f60b332a8f503ba54884175102aa0
minerBRUH <112920598+macalalayjc@users.noreply.github.com>
2024-04-19 13:13:18 +0800

    Interactive gallery button

--- frontend/src/components/PhotoBooth/GalleryButton.jsx
+++ frontend/src/components/PhotoBooth/GalleryButton.jsx
@@ -3,17 +3,49 @@ import { FaImage } from 'react-icons/fa';
 
 const GalleryButton = ({ onCapture }) => {
   const [showTooltip, setShowTooltip] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
 
   const handleButtonClick = () => {
     setShowTooltip(true);
+    setClickCount(prevCount => prevCount + 1);
     setTimeout(() => {
       setShowTooltip(false);
-    }, 1000);
+    }, 1500);
   };
 
   const handleTooltipClose = () => {
     setShowTooltip(false);
   };
+
+  let tooltipMessage = 'w8, ginagawa pa pu.';
+  if (clickCount === 1) {
+    tooltipMessage = 'w8, ginagawa pa pu.sorna';
+  } else if (clickCount === 2) {
+    tooltipMessage = ' ginagawa pa pu.';
+  } else if (clickCount ===3) {
+    tooltipMessage = `ginagawa pa nga ih`;
+  } else if (clickCount ===4) {
+    tooltipMessage = `luh`;
+  }
+  else if (clickCount ===5) {
+    tooltipMessage = `ang kulit ah`;
+  }
+  else if (clickCount ===6) {
+    tooltipMessage = `sabing ginagawa pa`;
+  }
+
+  else if (clickCount ===7) {
+    tooltipMessage = `ayy sige, hindi ko tutuloy 'to`;
+  }
+
+  else if (clickCount ===8) {
+    tooltipMessage = `kulit, kaya ka iniiwan eh`;
+  }
+
+  else if (clickCount ===9) {
+    tooltipMessage = `jk sorry, w8 lang kasi`;
+    setClickCount(0)
+  }
 
   return (
     <div style={{ position: 'relative' }}>
@@ -30,7 +62,7 @@ const GalleryButton = ({ onCapture }) => {
           className="bg-transparent text-red-500 py-2 px-4 rounded absolute bottom-0 left-1/4 transform -translate-x-1/2"
           onClick={handleTooltipClose}
         >
-          w8, ginagawa pa pu. sorna
+          {tooltipMessage}
         </div>
       )}
     </div>
