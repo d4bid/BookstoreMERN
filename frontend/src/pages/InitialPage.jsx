a5f2c07010c8b8e430e964e14f889db921b46768
d4bid <dvdcabrito@gmail.com>
2024-04-19 13:47:18 +0800

    fetch images from db then display to slideshow

--- frontend/src/pages/InitialPage.jsx
+++ frontend/src/pages/InitialPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Slideshow from '../components/Home/Slideshow';
 import Card from '../components/Home/Card';
 import PhotoboothButton from '../components/Home/PhotoboothButton';
@@ -8,42 +8,28 @@ import { IoPeopleCircleSharp } from "react-icons/io5";
 import { IoInformationCircleSharp } from "react-icons/io5";
 import { IoNewspaperSharp } from "react-icons/io5";
 import { MdWork } from "react-icons/md";
+import axios from 'axios';
 
 const InitialPage = () => {
-  const slideshowImages = [
-    '../src/assets/Slider/1.jpg',
-    '../src/assets/Slider/2.jpg',
-    '../src/assets/Slider/3.jpg',
-    '../src/assets/Slider/4.jpg',
-    '../src/assets/Slider/5.jpg',
-    '../src/assets/Slider/6.jpg',
-    '../src/assets/Slider/7.jpg',
-    '../src/assets/Slider/8.jpg',
-    '../src/assets/Slider/9.jpg',
-    '../src/assets/Slider/10.jpg',
-    '../src/assets/Slider/11.jpg',
-    '../src/assets/Slider/12.jpg',
-    '../src/assets/Slider/13.jpg',
-    '../src/assets/Slider/14.jpg',
-    '../src/assets/Slider/15.jpg',
-    '../src/assets/Slider/16.jpg',
-    '../src/assets/Slider/17.jpg',
-    '../src/assets/Slider/18.jpg',
-    '../src/assets/Slider/19.jpg',
-    '../src/assets/Slider/20.jpg',
-    '../src/assets/Slider/21.jpg',
-    '../src/assets/Slider/22.jpg',
-    '../src/assets/Slider/23.jpg',
-    '../src/assets/Slider/24.jpg',
-    '../src/assets/Slider/25.jpg',
-    '../src/assets/Slider/26.jpg',
-
-  ];
-
+  const [slideshowImages, setSlideshowImages] = useState([]);
   const navigate = useNavigate();
   const [isAboutUsModalOpen, setIsAboutUsModalOpen] = useState(false);
   const [isNewsModalOpen, setIsNewsModalOpen] = useState(false);
   const [isCareersModalOpen, setIsCareersModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchSlideshowImages = async () => {
+      try {
+        const response = await axios.get('http://localhost:5555/slideshow');
+        const fetchedImages = response.data.data.map(image => `data:image/jpeg;base64,${image.image}`);
+        setSlideshowImages(fetchedImages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSlideshowImages();
+  }, []);
 
   const handlePhotoboothClick = () => {
     navigate('/photobooth');
