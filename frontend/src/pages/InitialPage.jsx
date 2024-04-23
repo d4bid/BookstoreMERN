3976a425ee811a9d9b5aa30dd4ab1ae5868402c4
Junmar Rosario <junmarr30@gmail.com>
2024-04-23 10:46:56 +0800

    initial page change bg

--- frontend/src/pages/InitialPage.jsx
+++ frontend/src/pages/InitialPage.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import Slideshow from '../components/Home/Slideshow';
-import Card from '../components/Home/Card';
-import PhotoboothButton from '../components/Home/PhotoboothButton';
-import UrlModal from '../components/UrlModal';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Slideshow from "../components/Home/Slideshow";
+import Card from "../components/Home/Card";
+import PhotoboothButton from "../components/Home/PhotoboothButton";
+import UrlModal from "../components/UrlModal";
+import { useNavigate } from "react-router-dom";
 import { IoPeopleCircleSharp } from "react-icons/io5";
 import { IoInformationCircleSharp } from "react-icons/io5";
 import { IoNewspaperSharp } from "react-icons/io5";
 import { MdWork } from "react-icons/md";
-import axios from 'axios';
+import axios from "axios";
+import backgroundImage from "../assets/1.png"; // Import the image
 
 const InitialPage = () => {
   const [slideshowImages, setSlideshowImages] = useState([]);
@@ -20,8 +21,12 @@ const InitialPage = () => {
   useEffect(() => {
     const fetchSlideshowImages = async () => {
       try {
-        const response = await axios.get('http://localhost:5555/slideshow/active');
-        const fetchedImages = response.data.data.map(image => `data:image/jpeg;base64,${image.image}`);
+        const response = await axios.get(
+          "http://localhost:5555/slideshow/active"
+        );
+        const fetchedImages = response.data.data.map(
+          (image) => `data:image/jpeg;base64,${image.image}`
+        );
         setSlideshowImages(fetchedImages);
       } catch (error) {
         console.log(error);
@@ -32,7 +37,7 @@ const InitialPage = () => {
   }, []);
 
   const handlePhotoboothClick = () => {
-    navigate('/photobooth');
+    navigate("/photobooth");
   };
 
   const handleAboutUsClick = () => {
@@ -60,16 +65,22 @@ const InitialPage = () => {
   };
 
   const handleClientsClick = () => {
-    navigate('/partners');
+    navigate("/partners");
   };
 
   const handleProductsClick = () => {
-    navigate('/partners');
+    navigate("/partners");
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center relative">
-
+    <div
+      className="min-h-screen flex flex-col justify-center relative"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <div className="flex-fill">
         <Slideshow images={slideshowImages} className="w-20vw" />
       </div>
@@ -78,11 +89,23 @@ const InitialPage = () => {
 
       <div className="relative flex justify-center">
         <div className="flex flex-wrap justify-center">
-          <Card title="Partners" icon={IoPeopleCircleSharp} onClick={handleClientsClick} />
+          <Card
+            title="Partners"
+            icon={IoPeopleCircleSharp}
+            onClick={handleClientsClick}
+          />
           {/* <Card title="Products" icon={IoPricetagsSharp} onClick={handleClientsClick} /> */}
           <Card title="Careers" icon={MdWork} onClick={handleCareersClick} />
-          <Card title="News" icon={IoNewspaperSharp} onClick={handleNewsClick} />
-          <Card title="About Us" icon={IoInformationCircleSharp} onClick={handleAboutUsClick} />
+          <Card
+            title="News"
+            icon={IoNewspaperSharp}
+            onClick={handleNewsClick}
+          />
+          <Card
+            title="About Us"
+            icon={IoInformationCircleSharp}
+            onClick={handleAboutUsClick}
+          />
         </div>
       </div>
 
@@ -114,6 +137,6 @@ const InitialPage = () => {
       />
     </div>
   );
-}
+};
 
 export default InitialPage;
--- frontend/src/pages/PhotoBooth/PBMainPage.jsx
+++ frontend/src/pages/PhotoBooth/PBMainPage.jsx
@@ -1,17 +1,16 @@
-import React, { useRef, useState, useEffect } from 'react';
-import axios from 'axios';
-import Webcam from 'react-webcam';
-import FrameSelector from '../../components/PhotoBooth/FrameSelector';
-import CaptureButton from '../../components/PhotoBooth/CaptureButton';
-import GalleryButton from '../../components/PhotoBooth/GalleryButton';
-import PreviewModal from '../../components/PhotoBooth/PreviewModal';
-import BackButton from '../../components/BackButtonHome';
-import Spinner from '../../components/Spinner';
-import { useSnackbar } from 'notistack';
-import hytecLogo from '../../assets/hytecLogo.png';
-import Timer from '../../components/PhotoBooth/Timer';
-import CountdownVideo from '../../assets/Countdown.mp4'; //
-
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import Webcam from "react-webcam";
+import FrameSelector from "../../components/PhotoBooth/FrameSelector";
+import CaptureButton from "../../components/PhotoBooth/CaptureButton";
+import GalleryButton from "../../components/PhotoBooth/GalleryButton";
+import PreviewModal from "../../components/PhotoBooth/PreviewModal";
+import BackButton from "../../components/BackButtonHome";
+import Spinner from "../../components/Spinner";
+import { useSnackbar } from "notistack";
+import hytecLogo from "../../assets/hytecLogo.png";
+import Timer from "../../components/PhotoBooth/Timer";
+import CountdownVideo from "../../assets/Countdown.mp4"; //
 
 const PBMainPage = () => {
   const webcamRef = useRef(null);
@@ -33,7 +32,7 @@ const PBMainPage = () => {
       // Simulate camera loading delay (You can replace this with actual camera loading logic)
       setTimeout(() => {
         setIsLoading(false);
-      }, 2000);
+      }, 1000);
     };
 
     loadCamera();
@@ -43,7 +42,6 @@ const PBMainPage = () => {
     setSelectedFrame(frame);
   };
 
-
   const handleCapture = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
 
@@ -51,8 +49,8 @@ const PBMainPage = () => {
     image.src = imageSrc;
 
     image.onload = () => {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
 
       canvas.width = image.width;
       canvas.height = image.height;
@@ -61,8 +59,8 @@ const PBMainPage = () => {
       ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
       // Create a new canvas for flipped image
-      const flippedCanvas = document.createElement('canvas');
-      const flippedCtx = flippedCanvas.getContext('2d');
+      const flippedCanvas = document.createElement("canvas");
+      const flippedCtx = flippedCanvas.getContext("2d");
 
       flippedCanvas.width = canvas.width;
       flippedCanvas.height = canvas.height;
@@ -70,11 +68,17 @@ const PBMainPage = () => {
       // Flip the image horizontally
       flippedCtx.translate(flippedCanvas.width, 0);
       flippedCtx.scale(-1, 1);
-      flippedCtx.drawImage(image, 0, 0, flippedCanvas.width, flippedCanvas.height);
+      flippedCtx.drawImage(
+        image,
+        0,
+        0,
+        flippedCanvas.width,
+        flippedCanvas.height
+      );
 
       if (selectedFrame) {
-        const frameCanvas = document.createElement('canvas');
-        const frameCtx = frameCanvas.getContext('2d');
+        const frameCanvas = document.createElement("canvas");
+        const frameCtx = frameCanvas.getContext("2d");
 
         frameCanvas.width = canvas.width;
         frameCanvas.height = canvas.height;
@@ -84,18 +88,30 @@ const PBMainPage = () => {
 
         frameImage.onload = () => {
           // Draw the flipped image onto the frame canvas
-          frameCtx.drawImage(flippedCanvas, 0, 0, frameCanvas.width, frameCanvas.height);
+          frameCtx.drawImage(
+            flippedCanvas,
+            0,
+            0,
+            frameCanvas.width,
+            frameCanvas.height
+          );
 
           // Draw the frame on top of the flipped image
-          frameCtx.drawImage(frameImage, 0, 0, frameCanvas.width, frameCanvas.height);
+          frameCtx.drawImage(
+            frameImage,
+            0,
+            0,
+            frameCanvas.width,
+            frameCanvas.height
+          );
 
-          const newImageSrc = frameCanvas.toDataURL('image/jpeg');
+          const newImageSrc = frameCanvas.toDataURL("image/jpeg");
           setCapturedImage(newImageSrc);
           handleSaveImage(newImageSrc); // Save the image
           setIsPreviewOpen(true);
         };
       } else {
-        const newImageSrc = flippedCanvas.toDataURL('image/jpeg');
+        const newImageSrc = flippedCanvas.toDataURL("image/jpeg");
         setCapturedImage(newImageSrc);
         handleSaveImage(newImageSrc); // Save the image
         setIsPreviewOpen(true);
@@ -109,11 +125,16 @@ const PBMainPage = () => {
 
   const handleSaveImage = async (imageData) => {
     try {
-      const response = await axios.post('http://localhost:5555/photos/save-image', { imageData });
+      const response = await axios.post(
+        "http://localhost:5555/photos/save-image",
+        { imageData }
+      );
       const { imagePath } = response.data;
-      enqueueSnackbar(`Image saved successfully at ${imagePath}`, { variant: 'success' });
+      enqueueSnackbar(`Image saved successfully at ${imagePath}`, {
+        variant: "success",
+      });
     } catch (error) {
-      enqueueSnackbar(`Failed to save image`, { variant: 'error' });
+      enqueueSnackbar(`Failed to save image`, { variant: "error" });
     }
   };
 
@@ -128,7 +149,7 @@ const PBMainPage = () => {
   const handleCountdownEnd = () => {
     // Trigger flash effect
     setFlashVisible(true);
- //setFlashVisible(false);
+    //setFlashVisible(false);
     setTimeout(() => {
       setFlashVisible(false);
       handleCapture(); // Capture photo
@@ -140,8 +161,6 @@ const PBMainPage = () => {
     }, 100); // Adjust the delay time as needed
   };
 
-
-
   if (isLoading) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-white-500">
@@ -150,57 +169,67 @@ const PBMainPage = () => {
     );
   }
   return (
-    <div className="flex flex-col items-center justify-center bg-white-500" style={{ touchAction: 'none', msTouchAction: 'none', minHeight: '100vh' }}>
-
+    <div
+      className="flex flex-col items-center justify-center bg-white-500"
+      style={{ touchAction: "none", msTouchAction: "none", minHeight: "100vh" }}
+    >
       {/* <div className="absolute top-0 left-0 mt-4 ml-4" style={{width:'10vw', height:'auto'}}>
 
       </div> */}
 
       <div className="flex-grow"></div>
 
-      <div className="flex-grow" style={{ maxWidth: '40vw' }}>
+      <div className="flex-grow" style={{ maxWidth: "40vw" }}>
         <img src={hytecLogo} alt="Photo Booth" />
       </div>
 
       <div className="flex flex-col items-center justify-start">
-        <div className="relative mb-8" style={{ width: '80vw', maxWidth: '1536px' }}>
+        <div
+          className="relative mb-8"
+          style={{ width: "80vw", maxWidth: "1536px" }}
+        >
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            className="mb-4"
+            style={{
+              width: "100%",
+              height: "auto",
+              transform: "scaleX(-1)",
+              position: "relative",
+            }}
+          />
 
-        <Webcam
-    audio={false}
-    ref={webcamRef}
-    screenshotFormat="image/jpeg"
-    className="mb-4"
-    style={{ width: '100%', height: 'auto', transform: 'scaleX(-1)', position: 'relative' }}
-  />
-
-  {flashVisible && (
-    <div
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        background: 'white',
-        animation: 'flashAnimation 2s linear',
-      }}
-    />
-  )}
-
+          {flashVisible && (
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                background: "white",
+                animation: "flashAnimation 2s linear",
+              }}
+            />
+          )}
 
           {selectedFrame && (
             <img
               src={selectedFrame}
               alt="Selected Frame"
               className="absolute bottom-0 left-0 w-full h-full"
-              style={{ width: '100%' }}
+              style={{ width: "100%" }}
             />
           )}
         </div>
       </div>
 
       <div style={{ flex: 1 }}></div>
-      {showCountdown && <Timer durationInSeconds={7} onCountdownEnd={handleCountdownEnd} />}
+      {showCountdown && (
+        <Timer durationInSeconds={7} onCountdownEnd={handleCountdownEnd} />
+      )}
 
       {showFrameSelector && (
         <div className="mt-auto mb-8 ">
@@ -209,22 +238,13 @@ const PBMainPage = () => {
       )}
       <div style={{ flex: 1 }}></div>
       <div className="w-full flex items-center justify-between">
-
-        {showBackButton && (
-          <BackButton destination="/home" />
-        )}
-        <div className="flex-grow"></div> {/* Empty flex item to push the next component */}
-
-        {showCaptureButton && (
-
-          <CaptureButton onCapture={handleCountdown} />
-        )}
-
-        <div className="flex-grow"></div> {/* Empty flex item to push the last component */}
-
-        {showGalleryButton && (
-          <GalleryButton />
-        )}
+        {showBackButton && <BackButton destination="/home" />}
+        <div className="flex-grow"></div>{" "}
+        {/* Empty flex item to push the next component */}
+        {showCaptureButton && <CaptureButton onCapture={handleCountdown} />}
+        <div className="flex-grow"></div>{" "}
+        {/* Empty flex item to push the last component */}
+        {showGalleryButton && <GalleryButton />}
       </div>
 
       {isPreviewOpen && (
@@ -234,9 +254,8 @@ const PBMainPage = () => {
           onSave={handleSaveImage}
         />
       )}
-
     </div>
   );
-}
+};
 
 export default PBMainPage;
